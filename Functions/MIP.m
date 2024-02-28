56a511a40687c486c4a2406651aa6639c8e1f370
AviAdlakha <86912401+AviAdlakha@users.noreply.github.com>
2024-02-28 17:41:52 +0100

    Added exception catch incase it fails for some session, mode is now optional, if not supplied it means you want spatial footprint and not image, supply anything else for image

--- Functions/MIP.m
+++ Functions/MIP.m
@@ -1,26 +1,35 @@
-function MIP(Cohort,SelAnimals, mode)
+function MIP(Cohort,SelAnimals, vargin)
 %MIP_SP Get Max Image Projection, either from raw image or the final
 %spatial footprint, for the given cohort and animal.
 %   put spORmp = 1 for raw images, otherwise anything
 % or put it =2 for temporal images.
 
 Animals=fieldnames(Cohort);
+
 for a= SelAnimals
     Sessions=fieldnames(Cohort.(Animals{a}));
-    for i= 1:size(Sessions)
-        SessionNumbers=fieldnames(Cohort.(Animals{a}).(Sessions{i}));
-        for j= 1:size(SessionNumbers)
-            if mode == 1
-                MIP=cat(3,Cohort.(Animals{a}).(Sessions{i}).(SessionNumbers{j}).MaxProjection);
-            else
-                MIP=cat(3,Cohort.(Animals{a}).(Sessions{i}).(SessionNumbers{j}).SpatialFootprints);
+    for s= 1:size(Sessions)
+        SessionNumbers=fieldnames(Cohort.(Animals{a}).(Sessions{s}));
+        for n= 1:size(SessionNumbers)
+            try
+                if ~isempty(varargin)
+                    MIP=cat(3,Cohort.(Animals{a}).(Sessions{s}).(SessionNumbers{n}).CalciumData.MaxProjection);
+                else
+                    MIP=cat(3,Cohort.(Animals{a}).(Sessions{s}).(SessionNumbers{n}).CalciumData.SpatialFootprints);
+                end
+                
+            catch exception
+                fprintf("function failed for Animal %s, Session %s , Number %s ", (Animals{a}), (Sessions{s}), (SessionNumbers{n}))
+                disp(exception.message)
             end
+            
         end
+        
     end
-    
     figure
     imagesc(sum(MIP,3))
     title('Max Image Projection of:', (Animals{a}))
+    clearvars MIP
 end
 end
 
